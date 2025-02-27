ed1a67ef4a457f4ed63479b0953e5ad2c74131c0
ShariqShaikh33 <shariqshaikh2705@gmail.com>
2025-02-27 21:58:47 +0530

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,44 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [list, setList] = useState([]);
+  const addTask=(event)=>{
+    event.preventDefault();
+    let taskname=event.target.taskInput.value;
+    let id=crypto.randomUUID();
+    setList([...list,{name: taskname, id: id}]);
+  }
+  const deleteTask=(id)=>{
+    setList(list.filter((e)=>{
+      return e.id!=id;
+    }))
+    console.log(list);
+
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <div className="formDiv">
+          <form className="form" onSubmit={addTask}>
+            <input id="taskInput"></input>
+            <button type="submit">Add Task</button>
+          </form>
+        </div>
+        <div className="taskListDiv">
+        {
+          list.map((e)=>{
+            return(
+              <div  key={e.id}>
+                <p className="INLINE">{e.name}</p>
+                <button className="INLINE" onClick={()=>(deleteTask(e.id))}>Delete</button>
+              </div>
+              
+            ) 
+          })
+        }
+        </div>
     </div>
   )
 }
